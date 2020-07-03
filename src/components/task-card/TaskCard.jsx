9912c248a1952b297b96a15f5daeffb943356d03
Jingshuai-cloud <xujingshuai@gmail.com>
2020-07-03 18:55:50 +1000

    add all files

---
+++ src/components/task-card/TaskCard.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import axios from "axios";
+
+import styles from "./TaskCard.module.scss";
+
+class TaskCard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      task: [],
+      id: null,
+    };
+  }
+
+  // componentDidMount() {
+  //   axios.get(`http://localhost:8090/tasks`).then((tas) => {
+  //     const task = tas.data;
+  //     this.setState({ task });
+  //     console.log(task);
+  //     console.log("id:  " + this.state.id);
+  //     // const test = [{ name: "jingshuai", age: "13" }];
+  //     //console.log(task.title);
+  //   });
+  // }
+
+  handleClick = (taskId) => {
+    const id = taskId;
+    this.setState({ id });
+    console.log(id);
+  };
+
+  render() {
+    const { taskData } = this.props;
+
+    return (
+      <div className={styles.container}>
+        <ul className={styles.ul}>
+          {taskData.map((task) => (
+            <li key={task.taskId} className={styles.list}>
+              <a
+                className={styles.item}
+                onClick={() => this.props.getTaskId(task.taskId)}
+              >
+                <div className={styles.header}>
+                  <span className={styles.title}>{task.title}</span>
+                  <div className={styles.price}>
+                    <span>${task.price}</span>
+                  </div>
+                </div>
+
+                <div className={styles.body}>
+                  <div className={styles.bodyLeft}>
+                    <div className={styles.locationDateText}>
+                      {task.taskLocation}
+                    </div>
+                    <div className={styles.locationDateText}>
+                      {task.openDate}
+                    </div>
+                  </div>
+                  <div className={styles.avatar}>
+                    <img
+                      className={styles.avatarImg}
+                      alt=""
+                      src={require("./sunny.png")}
+                    ></img>
+                  </div>
+                </div>
+
+                <div className={styles.footer}>
+                  <span className={styles.status}>open</span>
+                </div>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default TaskCard;
